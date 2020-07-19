5e8b42684d23a4b9d4482fe1002976735b74ba41
hari <nepal9841921783@gmail.com>
2020-07-19 16:44:18 +0100

    added dir

--- server.js
+++ server.js
@@ -63,12 +63,15 @@ const cronJob = cron.job('0 0 * * *', async function(){
 cronJob.start();
 // schedule()
 
-const PORT=process.env.PORT||5000;
-const server=app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
-//handle unhandled promised rejections
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
-process.on('unhandledRejection',(err,promise)=>{
-    console.log(`error:${err.message}`)
-    server.close(()=>process.exit(1))
-});
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
